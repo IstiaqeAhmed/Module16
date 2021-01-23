f20816bef0d7310954102dcd76816f28aade69ab
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-01-24 03:29:29 +0600

    second

---
+++ fibonacci-advance.js
@@ -0,0 +1,41 @@
+// var name1 = " I am Istiaqe Ahmed Khan";
+//  console.log(name1.indexOf("Khan"))//zero(0) theke count kora suro korbe ebong number of index dekhabe.jemon ekhane 20 number index e khan ache. space keu count korbe.
+
+// var num1 = 45.5;
+// var num2 = 50;
+//  num1 = "" // num1 ke string kore kore dibe empty string deowar fole but num1 ke count korbe na. num1 total a count hbe na. only num2 er value dekhabe.
+// num2 = ""+num2;
+// num1 = + num1 ;//string er purbe sudomatro plus sign(+) diyeo string ke number type banano jai.
+// num1 = parseInt(num1);//string variable ke convert kore number type variable e rupantor korte parseFloat or parseInt(purno sonkha dekhabe doshomik chara) bebohar kora hoy.
+// total = num1 + num2;
+//console.log(typeof(num2))// type of() likhle variable type dekha jabe . string na number naki boolean ta jana jabe.
+// console.log(total)
+
+// num1 = 2.3334;
+// num2 = 3.5556;
+// add = num1 + num2;
+// console.log(add.toFixed(3));// .toFixed diye doshomik er pore koto ghor porjonto jabe ta dekha jabe.
+
+
+// var num = ["istiaqe", "ahmed", "khan", "is", "the", "best"];
+// console.log(num.indexOf("khan"))// space count kore na array er bitor theke.
+// var total = ((num.indexOf("ahmed"))+(num.indexOf("khan")));
+// console.log(total);
+
+// fibonacci series
+
+// // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// function fibonacciIterative(nume){
+//     var fibo = [1, 1];
+//     for(var i = 0; i<=nume; i++){
+
+//     }
+// }
+
+// var name = " istiaqe ";
+// console.log("istiaqe".length);// length er sahajje koto ti letter ache ta jana jai. 
+
+var name1 = ["istiaqe", "ahmed", "khan", "is", "the", "best"];
+// console.log(num.indexOf("khan"))// space count kore na array er bitor theke.
+
+}
